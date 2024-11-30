d84062856c1d89c7fd330f54a4733061850395b8
Kiwix <kiwix.shell@gmail.com>
2024-11-30 12:50:27 +0100

    Ajout du fichier main.c avec la template requise.

---
+++ main.c
@@ -0,0 +1,20 @@
+/* declaration de fonctionnalites supplementaires */
+#include <stdlib.h> /* EXIT_SUCCESS */
+#include <stdio.h> /* printf */
+
+/* declaration constantes et types utilisateurs */
+
+/* declaration de fonctions utilisateurs */
+
+/* fonction principale */
+int main()
+{
+    /* declaration et initialisation des variables */
+
+    /* ici faire quelque chose */
+
+    /* valeur fonction */
+    return EXIT_SUCCESS;
+}
+
+/* definitions des fonctions utilisateurs */
